47e2de1e7265338d72846c882e7b519ac5e3bf8c
P R <russphil.a@gmail.com>
2020-05-07 19:37:01 -0500

    Added evaluation of predictions from the optimal model on a test set.

--- tune_dense_model.py
+++ tune_dense_model.py
@@ -1,4 +1,5 @@
 from csv import reader
+from custom.model_development import build_dense_network, report
 from custom.model_development import getstats_fromstream, ImageDataset
 from custom.model_development import load_datasets, make_datasets
 from custom.model_development import RepeatedKFolds, SearchCV
@@ -31,20 +32,19 @@ dir_model_main = ('/home/bob/development/neural_nets/atomic_cloud/'
 
 path_model_id = Path(dir_model_main, f'num_ex_{num_ex[0]}_seed_{seed}')
 
-#make_datasets(class_names, datasets_dict, path_images_left, path_model_id, seed)
+make_datasets(class_names, datasets_dict, path_images_left, path_model_id, seed)
 
 num_pixels, scale_max, _ = getstats_fromstream(path_model_id,
                                                path_images_left)
-
 batch_size = 16
 epochs = 100
 val_count = 40
 
 model_params = {'input_dim': num_pixels,
-                'dense_layers': 1,
-                'nodes_per_layer': [[5, 10, 20]],
-                'hidden_act': ['relu']}
-#                'dropout_layers': [[0.15, 0.25]]}
+                'dense_layers': 2,
+                'nodes_per_layer': [[5, 10, 20], [5, 10, 20]],
+                'hidden_act': ['relu'],
+                'dropout_layers': [[0, 0.15, 0.25]]}
 
 modelDNN = Sequential()
 cv = RepeatedKFolds(n_splits=5, n_repeats=3, seed=11)
@@ -73,5 +73,49 @@ fit_params = {'path_data': path_images_left,
 search.fit(**fit_params)
 search.display_results(metrics)
 
-
+#Run this to check performance on the test set.
+#model_params = {'model': Sequential(),
+#                'input_dim': num_pixels,
+#                'dense_layers': 2,
+#                'nodes_per_layer': [5, 5],
+#                'hidden_act': 'relu',
+#                'dropout_layers': [0.15]}
+#model = build_dense_network(**model_params)
+#
+#model.compile(loss='binary_crossentropy',
+#              optimizer='adam',
+#              metrics=[AUC()])
+#
+#train_epochs = 15
+#ds_train_params = {
+#    'path_data': path_images_left,
+#    'path_model_id': path_model_id,
+#    'set_name': 'training',
+#    'batch_size': batch_size,
+#    'n_epochs': train_epochs,
+#    'rescale': 1/scale_max,
+#    'seed': 137}
+#ds = ImageDataset(**ds_train_params)
+#ds_train, _ = ds.get_datasets()
+#
+#steps_per_epoch = int(ds.n_examples // batch_size)
+#dnn_params = {'x': ds_train,
+#              'epochs': train_epochs,
+#              'verbose': 0,
+#              'steps_per_epoch': steps_per_epoch}
+#model.fit(**dnn_params)
+#
+#ds_test_params = {
+#    'path_data': path_images_left,
+#    'path_model_id': path_model_id,
+#    'set_name': 'test',
+#    'batch_size': batch_size,
+#    'rescale': 1/scale_max,
+#    'seed': 137}
+#
+#ds_test = ImageDataset(**ds_test_params).get_datasets()
+#
+#print("Detailed classification report:")
+#print()
+#print(report(model, ds_test[0], ds_test[1]))
 
